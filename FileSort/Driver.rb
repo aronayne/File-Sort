f7c3bab3b7a3dbfe89e940664b34ab1872d212e1
adrian.ronayne <adrian.ronayne@adrianronayne>
2011-10-28 15:05:48 +0100

    test

--- FileSort/Driver.rb
+++
@@ -1,7 +0,0 @@
-class Driver
-  
-  require 'Sort'
-  
-  s = Sort.new()
-  s.populateFilenames("c:\\test")
-end
---
+++ FileSort/Sort.rb
@@ -0,0 +1,83 @@
+
+require 'find'
+require 'fileutils'
+
+class Sort
+  
+  def initialize
+     @fileArray = Array.new()
+     @workingDir = "c:\\test"
+     @sortedFilesDir = "c:\\test\\files"
+   end
+
+  def createFolders()
+    
+    begin
+      FileUtils.remove_dir(@sortedFilesDir, true)
+      rescue Exception=>e
+      puts e
+    end
+    
+    Dir.mkdir(@sortedFilesDir)
+    dirToCreateArray = Array.new(('A'..'Z').to_a)
+    dirToCreateArray.each() {
+      |x|
+      Dir.mkdir(@sortedFilesDir+"\\"+x)
+    }
+  end
+  
+  def populateFilenames()
+  Find.find(@workingDir) do |path|
+       if FileTest.directory?(path)
+         if File.basename(path)[0] == ?.
+           Find.prune       # Don't look any further into this directory.
+         else
+         Dir.foreach(path) do
+         |f|
+         # do whatever you want with f, which is a filename within the
+         # given directory (not fully-qualified)
+           if !FileTest.directory? path + File::SEPARATOR + f
+             @fileArray.push(path + File::SEPARATOR + f)
+          # print "\n"+f
+           end
+         end
+           next
+         end
+       else
+       end
+     end
+end
+
+def sortFiles()
+  
+  directory_names = Dir.foreach(@sortedFilesDir)
+  matching_directory_names = @fileArray.map do |filename|
+    directory_names.find do |directory_name|
+     # puts directory_name+","+filename
+     # puts filename.upcase().to_a[3]
+      if(filename.upcase().start_with?(directory_name.upcase()))
+        
+        begin
+          
+          dirToCopyInto = @sortedFilesDir+"\\"+filename[/\/(.)(?!.*\/)/][1,1].upcase()
+          puts "Copying file "+filename+" To "+dirToCopyInto
+          FileUtils.cp filename, dirToCopyInto
+
+          rescue Exception=>e
+          puts e
+        end
+        
+
+      end
+    end
+  end
+end
+
+end
+
+
+s = Sort.new()
+
+s.populateFilenames()
+s.createFolders()
+s.sortFiles()
